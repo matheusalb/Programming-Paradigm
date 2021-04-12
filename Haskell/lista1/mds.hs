02929a6ff95efd3bb928b9fe71fcfd672fec0866
Matheus Albuquerque <mvca@cin.ufpe.br>
2021-04-12 15:03:57 -0300

    organized haskell file

--- Haskell/lista1/mds.hs
+++
@@ -1,9 +0,0 @@
-mdc :: Int -> Int -> Int
-mdc a b 
-    | b == 0        = a
-    | otherwise     = mdc b (mod a b)
-
-main = do
-   a <- readLn
-   b <- readLn
-   print (mdc (a :: Int) (b :: Int))
--- Haskell/lista1/soma_ate.hs
+++
@@ -1,6 +0,0 @@
-sumTo :: Int -> Int
-sumTo 0 = 0
-sumTo n = n + sumTo (n-1)
-
-main :: IO()
-main = interact $ show . sumTo . read
